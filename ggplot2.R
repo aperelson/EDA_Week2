c9ae613507f2c847227bf087f08bf8d93a21d66a
aperelson@gmail.com <aperelson@gmail.com>
2016-11-22 21:10:26 +0800

    ggplot

--- ggplot2.R
+++ ggplot2.R
@@ -23,3 +23,15 @@ qplot(displ, hwy, data = mpg, facets=.~drv)
 
 qplot(hwy, data = mpg, facets=drv~.,binwidth=2)
 
+
+## building up in layers:
+load("maacs.rda")
+head(maacs[, c("logpm25","bmicat","NocturnalSympt")])
+
+g <- ggplot(maacs, aes(logpm25,NocturnalSympt))
+p <- g + geom_point()
+print(p)
+
+
+
+
