21fed542ce24e162bb8ca3640afe07070d6878f3
aperelson <aperelson@gmail.com>
2016-11-22 20:17:23 +0800

    nnn

--- ggplot2.R
+++ ggplot2.R
@@ -1,1 +1,25 @@
 library(ggplot2)
+str(mpg)
+
+## Scatterplot uses two values - x & y:
+
+qplot(displ, hwy, data=mpg)
+
+qplot(displ, hwy, data=mpg, color=drv)
+
+qplot(displ, hwy, data=mpg, geom = c("point","smooth"))
+
+qplot(displ, hwy, data=mpg, fill=drv)
+
+
+## Histogram only uses one value:
+
+qplot(hwy, data=mpg, fill=drv)
+
+
+## Facets:
+
+qplot(displ, hwy, data = mpg, facets=.~drv)
+
+qplot(hwy, data = mpg, facets=drv~.,binwidth=2)
+
